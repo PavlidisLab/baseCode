236310da6a806ab9a7ee556deeda595a9cfef222
wbraynen <>
2004-06-01 22:18:17 +0000

    Color maps are now implemented.

--- src/baseCode/gui/ColorMatrix.java
+++ src/baseCode/gui/ColorMatrix.java
@@ -19,26 +19,30 @@ public class ColorMatrix {
 
     // data fields
     Color[][] m_colors;
-    Color[] m_colorMap;
-    final int m_totalColors = 64;
-    
+    Color[] m_colorPalette;
+    final int m_suggestedNumberOfColors = 64;
+
     DenseDoubleMatrix2DNamed m_matrix;
     DoubleMatrixReader m_matrixReader;
-       
+
     double m_minValue, m_maxValue;
     int m_totalRows, m_totalColumns;
 
-    String[] blackbody = {"black", "darkred", "orange", "yellow", "white"};
-    String[] spectrum  = {"darkred", "red", "orange", "yellow", "green", "blue", "darkblue", "violet"};
-    String[] redgreen  = {"red", "darkred", "black", "darkgreen", "green"};
-    String[] greenred  = {"green", "darkgreen", "black", "darkred", "red"};
+    // colors and color maps
+    final Color DARK_RED = new Color( 128, 0, 0 );
+    Color m_minColor = Color.green; // default
+    Color m_maxColor = Color.red;   // default
+    Color[] m_customColorMap    = { m_minColor, m_maxColor };
+    final Color[] GREENRED_COLORMAP  = { Color.green, Color.black, Color.red   };
+    final Color[] REDGREEN_COLORMAP  = { Color.red,   Color.black, Color.green };
+    final Color[] BLACKBODY_COLORMAP = { Color.black, DARK_RED, Color.orange, Color.yellow, Color.white };
+    Color[] m_currentColorMap = BLACKBODY_COLORMAP; // reference to a color map
 
-    
     public ColorMatrix() {
-        
+
         this( "C:\\1_normalized.txt" );
     }
-    
+
     /**
      * @param  filename  either an absolute path, or if providing a relative
      *                   path (e.g. data.txt), then keep in mind that it will
@@ -46,86 +50,123 @@ public class ColorMatrix {
      *                   (not my fault -- that's how java treats relative paths)
      */
     public ColorMatrix( String filename ) {
-        
+
         DenseDoubleMatrix2DNamed matrix = loadFile( filename );
         loadMatrix( matrix );
     }
-    
+
     public ColorMatrix( DenseDoubleMatrix2DNamed matrix ) {
 
         loadMatrix( matrix );
     }
-    
+
     /**
      * Calculate how fast we have to change color components.
      * Assume min and max colors are different!
+     *
+     * @param  minColor  red, green, or blue component of the RGB color
+     * @param  maxColor  red, green, or blue component of the RGB color
+     * @return  positive or negative step size
      */
-    double getStepSize( int minColor, int maxColor, int totalColors ) {
-        
+    int getStepSize( int minColor, int maxColor, int totalColors ) {
+
         int colorRange = maxColor - minColor;
-        return ((double)( colorRange ) / ( 1 == totalColors ? 1 : totalColors - 1 ));
-        // could be negative
-    } 
-    
-    
-    Color[] initColorMap() {
-        
-        final Color minColor = Color.green;
-        final Color maxColor = Color.red;
-        Color[] colorMap = new Color[m_totalColors];
+        double stepSize = colorRange / ( 1 == totalColors ? 1 : totalColors - 1 );
+        return (int)Math.round( stepSize );
+    }
 
-        int r = minColor.getRed();
-        int g = minColor.getGreen();
-        int b = minColor.getBlue();
-        
-        double redStepSize   = getStepSize( r, maxColor.getRed(),   m_totalColors );
-        double greenStepSize = getStepSize( g, maxColor.getGreen(), m_totalColors );
-        double blueStepSize  = getStepSize( b, maxColor.getBlue(),  m_totalColors );        
-        
-        
-        // allocate colors across a range
-        for (int i = 0;  i < m_totalColors;  i++)
+    /**
+     * Allocates colors across a range.
+     *
+     * @param suggestedNumberOfColors  palette resolution; if colorPalette.length
+     *        does not evenly divide into this number, the actual number of
+     *        colors in the palette will be rounded down.
+     * @param colorPalette  the simplest color map is { minColor, maxColor };
+     *                  you might, however, want to go through intermediate
+     *                  colors instead of following a straight-line route
+     *                  through the color space.
+     * @return Color[]  the color palette
+     */
+    Color[] initColorPalette( int suggestedNumberOfColors, Color[] colorPalette ) {
+
+        Color minColor;
+        Color maxColor;
+
+        // number of segments is one less than the number of points
+        // dividing the line into segments;  the color map contains points,
+        // not segments, so for example, if the color map is trivially
+        // { minColor, maxColor }, then there is only one segment
+        int totalSegments = m_currentColorMap.length - 1;
+
+        // allocate colors across a range; distribute evenly
+        // between intermediate points, if there are any
+        int colorsPerSegment = suggestedNumberOfColors / totalSegments;
+
+        // make sure all segments are equal by rounding down
+        // the total number of colors if necessary
+        int totalColors = totalSegments * colorsPerSegment;
+
+        // create color map to return
+        colorPalette = new Color[totalColors];
+
+        for (int segment = 0;  segment < totalSegments;  segment++)
         {
-            // clip
-            r = Math.min( r, 255 );
-            g = Math.min( g, 255 );
-            b = Math.min( b, 255 );
-            
-            // but also make sure it's not less than zero
-            r = Math.max( r, 0 );
-            g = Math.max( g, 0 );
-            b = Math.max( b, 0 );            
+           // the minimum color for each segment as defined by the current color map
+           minColor = m_currentColorMap[segment];
+           int r = minColor.getRed();
+           int g = minColor.getGreen();
+           int b = minColor.getBlue();
 
-            colorMap[i] = new Color( r, g, b );
-            
-            r += redStepSize;
-            g += greenStepSize;
-            b += blueStepSize;            
+           // the maximum color for each segment and the step sizes
+           maxColor = m_currentColorMap[segment + 1];
+           int redStepSize   = getStepSize( r, maxColor.getRed(),   colorsPerSegment );
+           int greenStepSize = getStepSize( g, maxColor.getGreen(), colorsPerSegment );
+           int blueStepSize  = getStepSize( b, maxColor.getBlue(),  colorsPerSegment );
+
+           for (int k, i=0;  i < colorsPerSegment;  i++)
+           {
+               // clip
+               r = Math.min( r, 255 );
+               g = Math.min( g, 255 );
+               b = Math.min( b, 255 );
+
+               // but also make sure it's not less than zero
+               r = Math.max( r, 0 );
+               g = Math.max( g, 0 );
+               b = Math.max( b, 0 );
+
+               k = segment * colorsPerSegment + i;
+               colorPalette[k] = new Color( r, g, b );
+
+               r += redStepSize;
+               g += greenStepSize;
+               b += blueStepSize;
+           }
         }
-        
-        return colorMap;
-        
-    } // end initColorMap
-    
-    
-    public void initColors() 
-    {        
-        m_colorMap = initColorMap();
+
+        return colorPalette;
+
+    } // end initColorPalette
+
+
+    public void initColors()
+    {
+        m_colorPalette = initColorPalette( m_suggestedNumberOfColors, m_currentColorMap );
         double range = m_maxValue - m_minValue;
-        if (0.0 == range) 
+        if (0.0 == range)
         {
             System.err.println( "Warning: range of values in data is zero." );
             range = 1.0; // This avoids getting a step size of zero
                          // in case all values in the matrix are equal.
         }
-        
+
         // map values to colors
         for (int row = 0;  row < m_totalRows;  row++)
         {
             for (int column = 0;  column < m_totalColumns;  column++)
             {
                 double value = m_matrix.get( row, column );
-                
+
                 // if values can be less than zero, shift them up
                 // to the range [0, maxValue + minValue]
                 if (m_minValue < 0)
@@ -133,12 +174,12 @@ public class ColorMatrix {
                     value += Math.abs( m_minValue );
                 }
                 // normalize the values to the range [0, totalColors]
-                int i = (int)(( m_totalColors / range ) * value );
-                m_colors[row][column] = m_colorMap[i];
+                int i = (int)(( (m_colorPalette.length-1) / range ) * value );
+                m_colors[row][column] = m_colorPalette[i];
             }
-        }        
+        }
     } // end initColors
-    
+
 
     public int getRowCount() {
 
@@ -159,8 +200,8 @@ public class ColorMatrix {
 
         return m_matrix.getRowName( i );
     }
-    
-    public void loadMatrix( DenseDoubleMatrix2DNamed matrix ) { 
+
+    public void loadMatrix( DenseDoubleMatrix2DNamed matrix ) {
 
         m_matrix = matrix;
         m_totalRows = m_matrix.rows();
@@ -179,13 +220,13 @@ public class ColorMatrix {
             }
         }
 
-        initColors();        
+        initColors();
     }
-    
+
     public DenseDoubleMatrix2DNamed loadFile( String filename ) {
-    
+
         m_matrixReader = new DoubleMatrixReader();
         DenseDoubleMatrix2DNamed matrix = (DenseDoubleMatrix2DNamed) m_matrixReader.read( filename );
         return matrix;
-    }    
+    }
 }
