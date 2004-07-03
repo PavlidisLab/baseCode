c99bc41f383dd32d5f9747d926af81da15a0b948
pavlidis <>
2004-07-03 10:24:35 +0000

    alert icons

--- src/baseCode/gui/StatusJlabel.java
+++ src/baseCode/gui/StatusJlabel.java
@@ -1,41 +1,68 @@
 package baseCode.gui;
 
 import java.awt.EventQueue;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.io.IOException;
+import java.io.InputStream;
 
 import javax.swing.JLabel;
 
 import baseCode.util.StatusViewer;
 
 /**
- * <p>Title: </p>
- * <p>Description: </p>
- * <p>Copyright: Copyright (c) 2003</p>
- * <p>Company: </p>
+ * <p>
+ * Title:
+ * </p>
+ * <p>
+ * Description:
+ * </p>
+ * <p>
+ * Copyright: Copyright (c) 2003
+ * </p>
+ * <p>
+ * Company:
+ * </p>
+ * 
  * @author Paul Pavlidis
  * @version $Id$
  */
 
-public class StatusJlabel implements StatusViewer {
+public class StatusJlabel extends JLabel implements StatusViewer {
    private JLabel jlabel;
 
-   public StatusJlabel(JLabel l) {
+   Image alertIcon = null;
+
+   public StatusJlabel( JLabel l ) {
+      InputStream in = getClass().getResourceAsStream( "alert.gif" );
+
+      byte[] buffer = null;
+      try {
+         buffer = new byte[in.available()];
+         in.read( buffer );
+      } catch ( IOException e ) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
+
+      alertIcon = Toolkit.getDefaultToolkit().createImage( buffer );
       this.jlabel = l;
    }
 
-   public void setStatus(String s) {
+   public void setStatus( String s ) {
       final String message = s;
-      System.err.println(s);
-//      try {
-//         Thread.sleep(100);
-//      } catch (InterruptedException ex) {
-//      }
+      System.err.println( s );
+      try {
+         Thread.sleep( 20 );
+      } catch ( InterruptedException ex ) {//      }
 
-      EventQueue.invokeLater(new Runnable() {
-         public void run() {
-            if (jlabel != null) {
-               jlabel.setText(message);
+         EventQueue.invokeLater( new Runnable() {
+            public void run() {
+               if ( jlabel != null ) {
+                  jlabel.setText( message );
+               }
             }
-         }
-      });
+         } );
+      }
    }
 }
