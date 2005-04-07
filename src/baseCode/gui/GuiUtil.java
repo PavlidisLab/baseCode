96ea2a37a8886d36ee59ed69c7c44f782b01eb06
pavlidis <>
2005-04-07 20:43:08 +0000

    minor

--- src/baseCode/gui/GuiUtil.java
+++ src/baseCode/gui/GuiUtil.java
@@ -15,51 +15,49 @@ import baseCode.util.FileTools;
 
 public class GuiUtil {
 
-   protected static final Log log = LogFactory.getLog( GuiUtil.class );
+    protected static final Log log = LogFactory.getLog( GuiUtil.class );
 
-   /**
-    * @param message
-    * @param e
-    */
-   public static void error( String message, Exception e ) {
-      JOptionPane.showMessageDialog( null, "Error: " + message + "\n" + e,
-            "Error", JOptionPane.ERROR_MESSAGE );
-      log.error( e );
-      e.printStackTrace();
-   }
+    /**
+     * @param message
+     * @param e
+     */
+    public static void error( String message, Exception e ) {
+        JOptionPane.showMessageDialog( null, "Error: " + message + "\n" + e, "Error", JOptionPane.ERROR_MESSAGE );
+        log.error( e );
+        e.printStackTrace();
+    }
 
-   /**
-    * @param message
-    */
-   public static void error( String message ) {
-      
-      JOptionPane.showMessageDialog( null, "Error: " + message + "\n", "Error",
-            JOptionPane.ERROR_MESSAGE );
-      log.error( message );
-   }
+    /**
+     * @param message
+     */
+    public static void error( String message ) {
+        JOptionPane.showMessageDialog( null, "Error: " + message + "\n", "Error", JOptionPane.ERROR_MESSAGE );
+        log.error( message );
+    }
 
-   /**
-    * @param filename
-    * @return @see baseCode.util.FileTools#checkPathIsReadableFile(String)
-    */
-   public static boolean testFile( String filename ) {
-      if ( !FileTools.testFile( filename ) ) {
-         error( "A required file field is not valid." );
-         return false;
-      }
-      return false;
-   }
+    /**
+     * @param filename
+     * @return
+     * @see baseCode.util.FileTools#checkPathIsReadableFile(String)
+     */
+    public static boolean testFile( String filename ) {
+        if ( !FileTools.testFile( filename ) ) {
+            error( "A required file field is not valid." );
+            return false;
+        }
+        return false;
+    }
 
-   /**
-    * @param text
-    * @return
-    */
-   public static boolean testDir( String filename ) {
-      if ( !FileTools.testDir( filename ) ) {
-         error( "A required directory field is not valid." );
-         return false;
-      }
-      return true;
-   }
+    /**
+     * @param text
+     * @return
+     */
+    public static boolean testDir( String filename ) {
+        if ( !FileTools.testDir( filename ) ) {
+            error( "A required directory field is not valid." );
+            return false;
+        }
+        return true;
+    }
 
 }
