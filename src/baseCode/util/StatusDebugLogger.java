2974c9b192f612f17cf60fb2f49be93bd64bb629
pavlidis <>
2005-04-27 03:32:13 +0000

    minor

--- src/baseCode/util/StatusDebugLogger.java
+++ src/baseCode/util/StatusDebugLogger.java
@@ -11,28 +11,30 @@ import org.apache.commons.logging.LogFactory;
  */
 public class StatusDebugLogger implements StatusViewer {
 
-   protected static final Log log = LogFactory.getLog( StatusDebugLogger.class );
+    protected static final Log log = LogFactory.getLog( StatusDebugLogger.class );
 
-   /*
-    * (non-Javadoc)
-    * 
-    * @see baseCode.util.StatusViewer#setStatus(java.lang.String)
-    */
-   public void setStatus( String s ) {
-      log.info( s );
-   }
+    /*
+     * (non-Javadoc)
+     * 
+     * @see baseCode.util.StatusViewer#setStatus(java.lang.String)
+     */
+    public void setStatus( String s ) {
+        if ( s.length() == 0 ) return;
+        log.info( s );
+    }
 
-   /*
-    * (non-Javadoc)
-    * 
-    * @see baseCode.util.StatusViewer#setError(java.lang.String)
-    */
-   public void setError( String s ) {
-      log.error( s );
-   }
+    /*
+     * (non-Javadoc)
+     * 
+     * @see baseCode.util.StatusViewer#setError(java.lang.String)
+     */
+    public void setError( String s ) {
+        if ( s.length() == 0 ) return;
+        log.error( s );
+    }
 
-   public void clear() {
-      // don't need to do anything.
-   }
+    public void clear() {
+        // don't need to do anything.
+    }
 
 }
