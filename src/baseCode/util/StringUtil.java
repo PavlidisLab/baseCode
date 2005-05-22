f60e4ed0a0e35a5de0ed04ca6d856d880252f419
pavlidis <>
2005-05-22 13:45:55 +0000

    method to wrap a string

--- src/baseCode/util/StringUtil.java
+++ src/baseCode/util/StringUtil.java
@@ -21,4 +21,46 @@ public class StringUtil {
         String[][] parsedFields = CSVParser.parse( line );
         return parsedFields[0];
     }
+
+    /**
+     * Method to wrap lines. It attempts to wrap on whitespace - it will not break within a word.
+     * 
+     * @param string
+     * @param charPerLine
+     * @param delim The string to put at the end of each new line. For example, "\n" or "&lt;br /&gt;";
+     * @return
+     */
+    public static String wrap( String string, int charPerLine, String delim ) {
+        if ( string == null || string.length() <= charPerLine ) return string;
+        StringBuffer buf = new StringBuffer();
+        char[] chars = string.toCharArray();
+        int lastLineCount = 0;
+        boolean needDelim = false;
+        for ( int i = 0; i < chars.length; i++ ) {
+            char c = chars[i];
+            buf.append( c );
+            lastLineCount++;
+
+            if ( needDelim ) {
+                if ( ( c == ' ' || c == '\t' || c == '\n' ) ) {
+                    buf.append( delim );
+                    lastLineCount = 0;
+                    needDelim = false;
+                    continue;
+                }
+            }
+
+            if ( i > 0 && lastLineCount >= charPerLine ) {
+                if ( ( c == ' ' || c == '\t' || c == '\n' ) ) {
+                    buf.append( delim );
+                    lastLineCount = 0;
+                    needDelim = false;
+                } else {
+                    needDelim = true;
+                }
+            }
+        }
+        return buf.toString();
+    }
+
 }
