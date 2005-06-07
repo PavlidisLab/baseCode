4e5abb2169965c4c67a4d5cb667fac149ce9e897
pavlidis <>
2005-06-07 09:49:41 +0000

    compatibility

--- src/baseCode/algorithm/learning/unsupervised/RankProp.java
+++ src/baseCode/algorithm/learning/unsupervised/RankProp.java
@@ -1,3 +1,23 @@
+/*
+ * The baseCode project
+ * 
+ * Copyright (c) 2005 Columbia University
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
 package baseCode.algorithm.learning.unsupervised;
 
 import baseCode.algorithm.Algorithm;
@@ -19,102 +39,100 @@ import com.braju.beta.format.Parameters;
  */
 public class RankProp extends Algorithm {
 
-   double alpha = 0.95; // alpha parameter, controls amount of "clustering"
-   int maxIter = 20;// number of iterations of algorithm
+    double alpha = 0.95; // alpha parameter, controls amount of "clustering"
+    int maxIter = 20;// number of iterations of algorithm
 
-   /**
-    * @param matrix
-    * @param query
-    * @param k
-    * @return
-    */
-   public DoubleMatrix1D computeRanking( AbstractNamedDoubleMatrix matrix,
-         AbstractNamedDoubleMatrix query, int k ) {
+    /**
+     * @param matrix
+     * @param query
+     * @param k
+     * @return
+     */
+    public DoubleMatrix1D computeRanking( AbstractNamedDoubleMatrix matrix, AbstractNamedDoubleMatrix query, int k ) {
 
-      DoubleMatrix1D yorig = new DenseDoubleMatrix1D( query.viewRow( 0 )
-            .toArray() );
-      
-      return this.computeRanking(matrix, yorig, k);
+        DoubleMatrix1D yorig = new DenseDoubleMatrix1D( query.viewRow( 0 ).toArray() );
 
-   }
+        return this.computeRanking( matrix, yorig, k );
 
- 
-   /**
-    * @param matrix
-    * @param matrix1D
-    * @param k
-    * @return
-    */
-   public DoubleMatrix1D computeRanking( AbstractNamedDoubleMatrix matrix, DoubleMatrix1D query, int indexOfQuery ) {
-      int dim = query.size();
-      DoubleMatrix1D y = new DenseDoubleMatrix1D( dim ); // we use own implementation for performance.s
-      DoubleMatrix1D yold = new DenseDoubleMatrix1D( dim );
+    }
 
-      if (query.size() <= 1) {
-         return null;
-      }
-      
-      y.assign( 0.0 ); // set all to zero.
-      y.setQuick( indexOfQuery, 1.0 );
+    /**
+     * @param matrix
+     * @param matrix1D
+     * @param k
+     * @return
+     */
+    public DoubleMatrix1D computeRanking( AbstractNamedDoubleMatrix matrix, DoubleMatrix1D query, int indexOfQuery ) {
+        int dim = query.size();
+        DoubleMatrix1D y = new DenseDoubleMatrix1D( dim ); // we use own implementation for performance.s
+        DoubleMatrix1D yold = new DenseDoubleMatrix1D( dim );
 
-      if ( alpha == 0.0 ) {
-         return query;
-      }
+        if ( query.size() <= 1 ) {
+            return null;
+        }
 
-      for ( int loops = 0; loops < maxIter; loops++ ) { // iterations of propagation
+        y.assign( 0.0 ); // set all to zero.
+        y.setQuick( indexOfQuery, 1.0 );
 
-         yold.assign( y ); // initially all zero except for 1 at the query point.
+        if ( alpha == 0.0 ) {
+            return query;
+        }
 
-         int lim = Math.min( query.size(), matrix.rows() );
-         
-         for ( int j = 0; j < lim; j++ ) {
-            if ( j == indexOfQuery ) continue; // don't update query
+        for ( int loops = 0; loops < maxIter; loops++ ) { // iterations of propagation
 
-            double dotProduct = matrix.viewRow( j ).zDotProduct( yold );
+            yold.assign( y ); // initially all zero except for 1 at the query point.
 
-            // new y is old y +
-            // new weighted linear combination of neighbors
-            y.set( j, ( alpha * dotProduct ) + query.getQuick( j ) );
-         }
+            int lim = Math.min( query.size(), matrix.rows() );
 
-         if ( loops % 5 == 0 ) {
-            log.info( " iteration " + loops + " y[0]="
-                  + Format.sprintf( "%g", new Parameters( y.getQuick( 0 ) ) ) );
-         }
-      }
-      return y;
-   }
-   
-   /**
-    * @return Returns the alpha.
-    */
-   public double getAlpha() {
-      return alpha;
-   }
+            for ( int j = 0; j < lim; j++ ) {
+                if ( j == indexOfQuery ) continue; // don't update query
 
-   /**
-    * controls amount of "clustering"
-    * @param alpha The alpha to set.
-    */
-   public void setAlpha( double alpha ) {
-      this.alpha = alpha;
-   }
+                double dotProduct = matrix.viewRow( j ).zDotProduct( yold );
 
-   /**
-    * Maximum iterations before stopping.
-    * @return Returns the max_loops.
-    */
-   public int getMaxIter() {
-      return maxIter;
-   }
+                // new y is old y +
+                // new weighted linear combination of neighbors
+                y.set( j, ( alpha * dotProduct ) + query.getQuick( j ) );
+            }
 
-   /**
-    * Maximum iterations before stopping.
-    * @param max_loops The max_loops to set.
-    */
-   public void setMaxIter( int maxIter ) {
-      this.maxIter = maxIter;
-   }
+            if ( loops % 5 == 0 ) {
+                log.info( " iteration " + loops + " y[0]=" + Format.sprintf( "%g", new Parameters( y.getQuick( 0 ) ) ) );
+            }
+        }
+        return y;
+    }
+
+    /**
+     * @return Returns the alpha.
+     */
+    public double getAlpha() {
+        return alpha;
+    }
+
+    /**
+     * controls amount of "clustering"
+     * 
+     * @param alpha The alpha to set.
+     */
+    public void setAlpha( double alpha ) {
+        this.alpha = alpha;
+    }
+
+    /**
+     * Maximum iterations before stopping.
+     * 
+     * @return Returns the max_loops.
+     */
+    public int getMaxIter() {
+        return maxIter;
+    }
+
+    /**
+     * Maximum iterations before stopping.
+     * 
+     * @param max_loops The max_loops to set.
+     */
+    public void setMaxIter( int maxIter ) {
+        this.maxIter = maxIter;
+    }
 
 }
-
