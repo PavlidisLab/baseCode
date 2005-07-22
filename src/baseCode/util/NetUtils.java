569760dd8d24b2f6b8795faa1318caf0eef159a2
pavlidis <>
2005-07-22 04:30:56 +0000

    more utilities

--- src/baseCode/util/NetUtils.java
+++ src/baseCode/util/NetUtils.java
@@ -20,12 +20,17 @@
  */
 package baseCode.util;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.SocketException;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
 import org.apache.commons.net.ftp.FTPReply;
 
 /**
@@ -66,5 +71,41 @@ public class NetUtils {
         f.setFileType( mode );
         log.info( "Connected to " + host );
         return f;
+    }
+
+    /**
+     * @param accession
+     * @param outputFileName
+     * @param outputFile
+     * @param seekFile
+     * @throws IOException
+     * @throws FileNotFoundException
+     */
+    public static boolean ftpDownloadFile( FTPClient f, String seekFile, String outputFileName, boolean force )
+            throws IOException, FileNotFoundException {
+        boolean success = false;
+        File outputFile = new File( outputFileName );
+        assert f != null && f.isConnected() : "No FTP connection is available";
+        FTPFile[] allfilesInGroup = f.listFiles( seekFile );
+        if ( allfilesInGroup.length == 0 ) {
+            log.error( "File " + seekFile + " does not seem to exist on the remote host" );
+            return false;
+        }
+
+        long expectedSize = allfilesInGroup[0].getSize();
+        if ( outputFile.exists() && outputFile.length() == expectedSize && !force ) {
+            log.warn( "Output file" + outputFileName + " already exists with correct size. Will not re-download" );
+            return true;
+        }
+
+        OutputStream os = new FileOutputStream( outputFileName );
+
+        log.info( "Seeking file " + seekFile + " with size " + expectedSize + " bytes" );
+        success = f.retrieveFile( seekFile, os );
+        os.close();
+        if ( !success ) {
+            log.error( "Failed to complete download of " + seekFile );
+        }
+        return success;
     }
 }
