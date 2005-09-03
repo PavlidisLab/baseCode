ac3b37d1539baa2a79bd06a5733bdae39ea1aeaf
pavlidis <>
2005-09-03 20:55:51 +0000

    make test conditional on server running

--- test/baseCode/util/RCommandTest.java
+++ test/baseCode/util/RCommandTest.java
@@ -1,4 +1,27 @@
+/*
+ * The baseCode project
+ * 
+ * Copyright (c) 2005 Columbia University
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
 package baseCode.util;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import junit.framework.TestCase;
 
@@ -11,11 +34,18 @@ import junit.framework.TestCase;
  * @version $Id$
  */
 public class RCommandTest extends TestCase {
+    private static Log log = LogFactory.getLog( RCommandTest.class.getName() );
 
     RCommand rc = null;
+    boolean connected = false;
 
     public void setUp() {
-        rc = RCommand.newInstance();
+        try {
+            rc = RCommand.newInstance();
+            connected = true;
+        } catch ( RuntimeException e ) {
+            connected = false;
+        }
     }
 
     public void tearDown() {
@@ -27,6 +57,10 @@ public class RCommandTest extends TestCase {
      * Test method for 'edu.columbia.gemma.tools.RCommand.exec(String)'
      */
     public void testExec() throws Exception {
+        if ( !connected ) {
+            log.warn( "Could not connect to RServe, skipping test." );
+            return;
+        }
         assertTrue( rc.eval( "R.version.string" ).asString().startsWith( "R version 2" ) );
     }
 
@@ -34,6 +68,10 @@ public class RCommandTest extends TestCase {
      * Test method for 'edu.columbia.gemma.tools.RCommand.exec(String)'
      */
     public void testExecDoubleArray() throws Exception {
+        if ( !connected ) {
+            log.warn( "Could not connect to RServe, skipping test." );
+            return;
+        }
         assertTrue( RegressionTesting.closeEnough( new double[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }, rc
                 .doubleArrayEval( "rep(1, 10)" ), 0.001 ) );
     }
