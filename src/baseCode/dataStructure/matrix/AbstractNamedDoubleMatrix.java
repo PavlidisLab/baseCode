8e065ee4e6fc852723ddbb701162094ba55b54e5
pavlidis <>
2005-09-05 18:11:48 +0000

    implement equals

--- src/baseCode/dataStructure/matrix/AbstractNamedDoubleMatrix.java
+++ src/baseCode/dataStructure/matrix/AbstractNamedDoubleMatrix.java
@@ -1,85 +1,112 @@
+/*
+ * The baseCode project
+ * 
+ * Copyright (c) 2005 Columbia University
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
 package baseCode.dataStructure.matrix;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import cern.colt.list.DoubleArrayList;
 import cern.colt.matrix.DoubleMatrix1D;
 
 /**
- * 
- *
  * <hr>
- * <p>Copyright (c) 2004 Columbia University
- * @author pavlidis
- * @version $Id$
- */
-/**
+ * <p>
+ * Copyright (c) 2004 Columbia University
  * 
- *
- * <hr>
- * <p>Copyright (c) 2004 Columbia University
  * @author pavlidis
  * @version $Id$
  */
 public abstract class AbstractNamedDoubleMatrix extends AbstractNamedMatrix {
 
-   /* (non-Javadoc)
-    * @see baseCode.dataStructure.matrix.NamedMatrix#rows()
-    */
-   public abstract int rows() ;
+    protected static Log log = LogFactory.getLog( DenseDoubleMatrix2DNamed.class.getName() );
 
-   /* (non-Javadoc)
-    * @see baseCode.dataStructure.matrix.NamedMatrix#columns()
-    */
-   public abstract int columns();
+    /*
+     * (non-Javadoc)
+     * 
+     * @see baseCode.dataStructure.matrix.NamedMatrix#rows()
+     */
+    public abstract int rows();
 
-   /* (non-Javadoc)
-    * @see baseCode.dataStructure.matrix.NamedMatrix#set(int, int, java.lang.Object)
-    */
-   public abstract void set( int i, int j, Object val );
+    /*
+     * (non-Javadoc)
+     * 
+     * @see baseCode.dataStructure.matrix.NamedMatrix#columns()
+     */
+    public abstract int columns();
 
-   /* (non-Javadoc)
-    * @see baseCode.dataStructure.matrix.NamedMatrix#getRowObj(int)
-    */
-   public abstract Object[] getRowObj( int i );
+    /*
+     * (non-Javadoc)
+     * 
+     * @see baseCode.dataStructure.matrix.NamedMatrix#set(int, int, java.lang.Object)
+     */
+    public abstract void set( int i, int j, Object val );
 
-   /* (non-Javadoc)
-    * @see baseCode.dataStructure.matrix.NamedMatrix#getColObj(int)
-    */
-   public abstract Object[] getColObj( int i );
+    /*
+     * (non-Javadoc)
+     * 
+     * @see baseCode.dataStructure.matrix.NamedMatrix#getRowObj(int)
+     */
+    public abstract Object[] getRowObj( int i );
 
-   /* (non-Javadoc)
-    * @see baseCode.dataStructure.matrix.NamedMatrix#isMissing(int, int)
-    */
-   public abstract boolean isMissing( int i, int j );
+    /*
+     * (non-Javadoc)
+     * 
+     * @see baseCode.dataStructure.matrix.NamedMatrix#getColObj(int)
+     */
+    public abstract Object[] getColObj( int i );
 
-   public abstract double[] getRow(int i);
-   
-   public abstract DoubleArrayList getRowArrayList(int i);
-   
-   
-   public abstract double  get(int x, int y );
-   
-   /**
-    * @param i
-    * @param j
-    * @return
-    */
-   public abstract double getQuick( int i, int j );
+    /*
+     * (non-Javadoc)
+     * 
+     * @see baseCode.dataStructure.matrix.NamedMatrix#isMissing(int, int)
+     */
+    public abstract boolean isMissing( int i, int j );
 
-   public abstract void set(int x, int y, double value);
-   
-   
-   /**
-    * @param j
-    * @return
-    */
-   public abstract DoubleMatrix1D viewRow( int j );
+    public abstract double[] getRow( int i );
 
-   /**
-    * @param s String
-    * @return double[]
-    */
-   public double[] getRowByName( String s ) {
-      return getRow( getRowIndexByName( s ) );
-   }
- 
+    public abstract DoubleArrayList getRowArrayList( int i );
+
+    public abstract double get( int x, int y );
+
+    /**
+     * @param i
+     * @param j
+     * @return
+     */
+    public abstract double getQuick( int i, int j );
+
+    public abstract void set( int x, int y, double value );
+
+    /**
+     * @param j
+     * @return
+     */
+    public abstract DoubleMatrix1D viewRow( int j );
+
+    /**
+     * @param s String
+     * @return double[]
+     */
+    public double[] getRowByName( String s ) {
+        return getRow( getRowIndexByName( s ) );
+    }
+
 }
--- src/baseCode/dataStructure/matrix/DenseDoubleMatrix2DNamed.java
+++ src/baseCode/dataStructure/matrix/DenseDoubleMatrix2DNamed.java
@@ -1,3 +1,23 @@
+/*
+ * The baseCode project
+ * 
+ * Copyright (c) 2005 Columbia University
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
 package baseCode.dataStructure.matrix;
 
 import cern.colt.list.DoubleArrayList;
@@ -20,249 +40,282 @@ import corejava.Format;
  */
 public class DenseDoubleMatrix2DNamed extends AbstractNamedDoubleMatrix {
 
-   private DenseDoubleMatrix2D matrix;
+    /**
+     * Value used to compare values in equals. This is used largely for testing and sanity checking.
+     */
+    private static final double TOLERANCE = 0.0001;
 
-   /**
-    * @param T double[][]
-    */
-   public DenseDoubleMatrix2DNamed( double T[][] ) {
-      super();
-      matrix = new DenseDoubleMatrix2D( T );
-   }
+    private DenseDoubleMatrix2D matrix;
 
-   /**
-    * @param rows int
-    * @param cols int
-    */
-   public DenseDoubleMatrix2DNamed( int rows, int cols ) {
-      super();
-      matrix = new DenseDoubleMatrix2D( rows, cols );
-   }
+    /**
+     * @param T double[][]
+     */
+    public DenseDoubleMatrix2DNamed( double T[][] ) {
+        super();
+        matrix = new DenseDoubleMatrix2D( T );
+    }
 
-   /**
-    * Return a reference to a specific row.
-    * 
-    * @param row int
-    * @return double[]
-    */
-   public double[] getRow( int row ) {
-      return viewRow( row ).toArray();
-   }
-   
-   
-   /**
-    * Return a reference to a specific row.
-    * @param s String
-    * @return double[]
-    */
-   public double[] getRowByName( String s ) {
-      return getRow( getRowIndexByName( s ) );
-   }
+    /**
+     * @param rows int
+     * @param cols int
+     */
+    public DenseDoubleMatrix2DNamed( int rows, int cols ) {
+        super();
+        matrix = new DenseDoubleMatrix2D( rows, cols );
+    }
 
-   /**
-    * Return a copy of a given column.
-    * 
-    * @param col int
-    * @return double[]
-    */
-   public double[] getCol( int col ) {
-      double[] result = new double[rows()];
-      for ( int i = 0; i < rows(); i++ ) {
-         result[i] = get( i, col );
-      }
-      return result;
-   }
-   
-   
-   /**
-    * Return a copy of a given column.
-    * 
-    * @param col int
-    * @return double[]
-    */
-   public double[] getColByName( String s ) {
-      int col = getColIndexByName(s);
-      double[] result = new double[rows()];
-      for ( int i = 0; i < rows(); i++ ) {
-         result[i] = get( i, col );
-      }
-      return result;
-   }
+    /**
+     * Return a reference to a specific row.
+     * 
+     * @param row int
+     * @return double[]
+     */
+    public double[] getRow( int row ) {
+        return viewRow( row ).toArray();
+    }
 
-   
+    /**
+     * Return a reference to a specific row.
+     * 
+     * @param s String
+     * @return double[]
+     */
+    public double[] getRowByName( String s ) {
+        return getRow( getRowIndexByName( s ) );
+    }
 
-   public Object[] getRowObj( int row ) {
-      Double[] result = new Double[columns()];
-      for ( int i = 0; i < columns(); i++ ) {
-         result[i] = new Double( get( row, i ) );
-      }
-      return result;
-   }
+    /**
+     * Return a copy of a given column.
+     * 
+     * @param col int
+     * @return double[]
+     */
+    public double[] getCol( int col ) {
+        double[] result = new double[rows()];
+        for ( int i = 0; i < rows(); i++ ) {
+            result[i] = get( i, col );
+        }
+        return result;
+    }
 
-   public Object[] getColObj( int col ) {
-      Double[] result = new Double[rows()];
-      for ( int i = 0; i < rows(); i++ ) {
-         result[i] = new Double( get( i, col ) );
-      }
-      return result;
-   }
+    /**
+     * Return a copy of a given column.
+     * 
+     * @param col int
+     * @return double[]
+     */
+    public double[] getColByName( String s ) {
+        int col = getColIndexByName( s );
+        double[] result = new double[rows()];
+        for ( int i = 0; i < rows(); i++ ) {
+            result[i] = get( i, col );
+        }
+        return result;
+    }
 
-   /**
-    * @return java.lang.String
-    */
-   public String toString() {
-      Format nf = new Format( "%.4g" );
-      StringBuffer result = new StringBuffer( this.rows() * this.columns() );
-      if ( this.hasColNames() || this.hasRowNames() ) {
-         result.append( "label" );
-      }
+    public Object[] getRowObj( int row ) {
+        Double[] result = new Double[columns()];
+        for ( int i = 0; i < columns(); i++ ) {
+            result[i] = new Double( get( row, i ) );
+        }
+        return result;
+    }
 
-      if ( this.hasColNames() ) {
-         for ( int i = 0; i < columns(); i++ ) {
-            result.append( "\t" + getColName( i ) );
-         }
-         result.append( "\n" );
-      }
+    public Object[] getColObj( int col ) {
+        Double[] result = new Double[rows()];
+        for ( int i = 0; i < rows(); i++ ) {
+            result[i] = new Double( get( i, col ) );
+        }
+        return result;
+    }
 
-      for ( int i = 0; i < rows(); i++ ) {
-         if ( this.hasRowNames() ) {
-            result.append( getRowName( i ) );
-         }
-         for ( int j = 0; j < columns(); j++ ) {
-            if ( Double.isNaN( get( i, j ) ) ) {
-               result.append( "\t" );
-            } else {
-               result.append( "\t" + nf.format( get( i, j ) ) );
+    /**
+     * @return java.lang.String
+     */
+    public String toString() {
+        Format nf = new Format( "%.4g" );
+        StringBuffer result = new StringBuffer( this.rows() * this.columns() );
+        if ( this.hasColNames() || this.hasRowNames() ) {
+            result.append( "label" );
+        }
+
+        if ( this.hasColNames() ) {
+            for ( int i = 0; i < columns(); i++ ) {
+                result.append( "\t" + getColName( i ) );
             }
-         }
-         result.append( "\n" );
-      }
-      return result.toString();
-   }
+            result.append( "\n" );
+        }
 
-  
-
-   public void set( int row, int col, Object value ) {
-      set( row, col, ( ( Double ) value ).doubleValue() );
-   }
-
-   /**
-    * Make a copy of a matrix.
-    * @return baseCode.dataStructure.DenseDoubleMatrix2DNamed
-    */
-   public DenseDoubleMatrix2DNamed copy() {
-      DenseDoubleMatrix2DNamed returnval = new DenseDoubleMatrix2DNamed( this
-            .rows(), this.columns() );
-      for ( int i = 0, n = this.rows(); i < n; i++ ) {
-         returnval.addRowName( this.getRowName( i ), i );
-         for ( int j = 0, m = this.columns(); j < m; j++ ) {
-            if ( i == 0 ) {
-               returnval.addColumnName( this.getColName( j ), j );
+        for ( int i = 0; i < rows(); i++ ) {
+            if ( this.hasRowNames() ) {
+                result.append( getRowName( i ) );
             }
-            returnval.set( i, j, this.get( i, j ) );
-         }
-      }
-      return returnval;
-   }
+            for ( int j = 0; j < columns(); j++ ) {
+                if ( Double.isNaN( get( i, j ) ) ) {
+                    result.append( "\t" );
+                } else {
+                    result.append( "\t" + nf.format( get( i, j ) ) );
+                }
+            }
+            result.append( "\n" );
+        }
+        return result.toString();
+    }
 
-   public boolean isMissing( int i, int j ) {
-      return Double.isNaN( get( i, j ) );
-   }
+    public void set( int row, int col, Object value ) {
+        set( row, col, ( ( Double ) value ).doubleValue() );
+    }
 
-   /**
-    * @param row int
-    * @param column int
-    * @return @see DoubleMatrix2D#get(int, int)
-    */
-   public double get( int row, int column ) {
-      return matrix.get( row, column );
-   }
+    /**
+     * Make a copy of a matrix.
+     * 
+     * @return baseCode.dataStructure.DenseDoubleMatrix2DNamed
+     */
+    public DenseDoubleMatrix2DNamed copy() {
+        DenseDoubleMatrix2DNamed returnval = new DenseDoubleMatrix2DNamed( this.rows(), this.columns() );
+        for ( int i = 0, n = this.rows(); i < n; i++ ) {
+            returnval.addRowName( this.getRowName( i ), i );
+            for ( int j = 0, m = this.columns(); j < m; j++ ) {
+                if ( i == 0 ) {
+                    returnval.addColumnName( this.getColName( j ), j );
+                }
+                returnval.set( i, j, this.get( i, j ) );
+            }
+        }
+        return returnval;
+    }
 
-   /**
-    * @param row int
-    * @param column int
-    * @return double
-    * @see DenseDoubleMatrix2D#getQuick(int, int)
-    */
-   public double getQuick( int row, int column ) {
-      return matrix.getQuick( row, column );
-   }
+    public boolean isMissing( int i, int j ) {
+        return Double.isNaN( get( i, j ) );
+    }
 
-   /**
-    * @param row int
-    * @param column int
-    * @param value double
-    */
-   public void set( int row, int column, double value ) {
-      matrix.set( row, column, value );
-   }
+    /**
+     * @param row int
+     * @param column int
+     * @return
+     * @see DoubleMatrix2D#get(int, int)
+     */
+    public double get( int row, int column ) {
+        return matrix.get( row, column );
+    }
 
-   /**
-    * @param row int
-    * @param column int
-    * @param value double
-    * @see DenseDoubleMatrix2D#setQuick(int, int, double)
-    */
-   public void setQuick( int row, int column, double value ) {
-      matrix.setQuick( row, column, value );
-   }
+    /**
+     * @param row int
+     * @param column int
+     * @return double
+     * @see DenseDoubleMatrix2D#getQuick(int, int)
+     */
+    public double getQuick( int row, int column ) {
+        return matrix.getQuick( row, column );
+    }
 
-   /**
-    * @param column int
-    * @return cern.colt.matrix.DoubleMatrix1D
-    */
-   public DoubleMatrix1D viewColumn( int column ) {
-      return matrix.viewColumn( column );
-   }
+    /**
+     * @param row int
+     * @param column int
+     * @param value double
+     */
+    public void set( int row, int column, double value ) {
+        matrix.set( row, column, value );
+    }
 
-   /**
-    * @param row int
-    * @return DoubleMatrix1D
-    * @see DenseDoubleMatrix2D#viewRow(int)
-    */
-   public DoubleMatrix1D viewRow( int row ) {
-      return matrix.viewRow( row );
-   }
+    /**
+     * @param row int
+     * @param column int
+     * @param value double
+     * @see DenseDoubleMatrix2D#setQuick(int, int, double)
+     */
+    public void setQuick( int row, int column, double value ) {
+        matrix.setQuick( row, column, value );
+    }
 
-   /**
-    * @return the number of columns in the matrix
-    * @see cern.colt.matrix.impl.AbstractMatrix2D#columns()
-    */
-   public int columns() {
-      return matrix.columns();
-   }
+    /**
+     * @param column int
+     * @return cern.colt.matrix.DoubleMatrix1D
+     */
+    public DoubleMatrix1D viewColumn( int column ) {
+        return matrix.viewColumn( column );
+    }
 
-   /**
-    * @return the number of rows in the matrix
-    * @see AbstractMatrix2D#rows()
-    */
-   public int rows() {
-      return matrix.rows();
-   }
+    /**
+     * @param row int
+     * @return DoubleMatrix1D
+     * @see DenseDoubleMatrix2D#viewRow(int)
+     */
+    public DoubleMatrix1D viewRow( int row ) {
+        return matrix.viewRow( row );
+    }
 
-   /**
-    * @return int
-    * @see AbstractMatrix2D#size()
-    */
-   public int size() {
-      return matrix.size();
-   }
+    /**
+     * @return the number of columns in the matrix
+     * @see cern.colt.matrix.impl.AbstractMatrix2D#columns()
+     */
+    public int columns() {
+        return matrix.columns();
+    }
 
-   /**
-    * @return double[][]
-    */
-   public double[][] toArray() {
-      return matrix.toArray();
-   }
+    /**
+     * @return the number of rows in the matrix
+     * @see AbstractMatrix2D#rows()
+     */
+    public int rows() {
+        return matrix.rows();
+    }
 
-   /* (non-Javadoc)
-    * @see baseCode.dataStructure.matrix.AbstractNamedDoubleMatrix#getRowArrayList(int)
-    */
-   public DoubleArrayList getRowArrayList( int i ) {
-      return new DoubleArrayList(getRow(i));
-   }
-   
-  
+    /**
+     * @return int
+     * @see AbstractMatrix2D#size()
+     */
+    public int size() {
+        return matrix.size();
+    }
 
-    
+    /**
+     * @return double[][]
+     */
+    public double[][] toArray() {
+        return matrix.toArray();
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see baseCode.dataStructure.matrix.AbstractNamedDoubleMatrix#getRowArrayList(int)
+     */
+    public DoubleArrayList getRowArrayList( int i ) {
+        return new DoubleArrayList( getRow( i ) );
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    public boolean equals( Object o ) {
+        if ( !( o instanceof DenseDoubleMatrix2DNamed ) ) return false;
+        DenseDoubleMatrix2DNamed d = ( DenseDoubleMatrix2DNamed ) o;
+        if ( d.rows() != this.rows() || d.columns() != this.columns() ) return false;
+
+        for ( int i = 0; i < this.columns(); i++ ) {
+            if ( !this.getColName( i ).equals( d.getColName( i ) ) ) {
+                log.info( "unequal column name" );
+                return false;
+            }
+        }
+
+        for ( int i = 0; i < this.rows(); i++ ) {
+            if ( !this.getRowName( i ).equals( d.getRowName( i ) ) ) {
+                log.info( "unequal row name " );
+                return false;
+            }
+            for ( int j = 0; j < this.columns(); j++ ) {
+                if ( Math.abs( d.getQuick( i, j ) - this.getQuick( i, j ) ) > TOLERANCE ) {
+                    log.info( "Unequal values " + d.getQuick( i, j ) + " and " + this.getQuick( i, j ) );
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
 }
