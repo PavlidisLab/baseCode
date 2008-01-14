d82342fa13a5627f2791888d60bf73c7b2fdbfd8
ppavlidis <paul@chibi.ubc.ca>
2008-01-14 18:05:00 +0000

    forgot to comment out

--- src/ubic/basecode/util/JRIClient.java
+++ src/ubic/basecode/util/JRIClient.java
@@ -40,31 +40,31 @@ public class JRIClient implements RClient {
 
     private static Log log = LogFactory.getLog( JRIClient.class.getName() );
 
-    public static void main( String[] args ) {
-        // just making sure we have the right version of everything
-        if ( !Rengine.versionCheck() ) {
-            System.err.println( "** Version mismatch - Java files don't match library version." );
-            System.exit( 1 );
-        }
-        System.out.println( "Creating Rengine (with arguments)" );
-        // 1) we pass the arguments from the command line
-        // 2) we won't use the main loop at first, we'll start it later
-        // (that's the "false" as second argument)
-        // 3) the callbacks are implemented by the TextConsole class above
-        Rengine re = new Rengine( args, false, new TextConsole() );
-        System.out.println( "Rengine created, waiting for R" );
-        // the engine creates R is a new thread, so we should wait until it's ready
-        if ( !re.waitForR() ) {
-            System.out.println( "Cannot load R" );
-            return;
-        }
-
-        REXP x;
-        re.eval( "data(iris)", false );
-        System.out.println( x = re.eval( "iris" ) );
-        re.end();
-
-    }
+//    public static void main( String[] args ) {
+//        // just making sure we have the right version of everything
+//        if ( !Rengine.versionCheck() ) {
+//            System.err.println( "** Version mismatch - Java files don't match library version." );
+//            System.exit( 1 );
+//        }
+//        System.out.println( "Creating Rengine (with arguments)" );
+//        // 1) we pass the arguments from the command line
+//        // 2) we won't use the main loop at first, we'll start it later
+//        // (that's the "false" as second argument)
+//        // 3) the callbacks are implemented by the TextConsole class above
+//        Rengine re = new Rengine( args, false, new TextConsole() );
+//        System.out.println( "Rengine created, waiting for R" );
+//        // the engine creates R is a new thread, so we should wait until it's ready
+//        if ( !re.waitForR() ) {
+//            System.out.println( "Cannot load R" );
+//            return;
+//        }
+//
+//        REXP x;
+//        re.eval( "data(iris)", false );
+//        System.out.println( x = re.eval( "iris" ) );
+//        re.end();
+//
+//    }
 
     Rengine connection;
 
