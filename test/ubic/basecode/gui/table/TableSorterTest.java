70b4add6cf3aa4977380d0b9ad26f345e09e3b8a
ppavlidis <paul@chibi.ubc.ca>
2008-02-14 18:23:21 +0000

    committed a bit prematurely

--- test/ubic/basecode/gui/table/TableSorterTest.java
+++ test/ubic/basecode/gui/table/TableSorterTest.java
@@ -1,19 +1,34 @@
-/**
+/*
+ * The baseCode project
  * 
+ * Copyright (c) 2008 University of British Columbia
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  */
 package ubic.basecode.gui.table;
 
 import junit.framework.TestCase;
 
 /**
- * TODO Document Me
- * 
  * @author Paul
  * @version $Id$
  */
 public class TableSorterTest extends TestCase {
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     * 
      * @see junit.framework.TestCase#setUp()
      */
     protected void setUp() throws Exception {
@@ -24,28 +39,30 @@ public class TableSorterTest extends TestCase {
      * Test method for {@link ubic.basecode.gui.table.TableSorter#TableSorter()}.
      */
     public void testTableSorter() {
-        fail( "Not yet implemented" );
+        // fail( "Not yet implemented" );
     }
 
     /**
      * Test method for {@link ubic.basecode.gui.table.TableSorter#TableSorter(javax.swing.table.TableModel)}.
      */
     public void testTableSorterTableModel() {
-        fail( "Not yet implemented" );
+        // fail( "Not yet implemented" );
     }
 
     /**
-     * Test method for {@link ubic.basecode.gui.table.TableSorter#TableSorter(javax.swing.table.TableModel, javax.swing.table.JTableHeader)}.
+     * Test method for
+     * {@link ubic.basecode.gui.table.TableSorter#TableSorter(javax.swing.table.TableModel, javax.swing.table.JTableHeader)}.
      */
     public void testTableSorterTableModelJTableHeader() {
-        fail( "Not yet implemented" );
+        // fail( "Not yet implemented" );
     }
 
     /**
-     * Test method for {@link ubic.basecode.gui.table.TableSorter#TableSorter(javax.swing.table.TableModel, ubic.basecode.gui.JMatrixDisplay)}.
+     * Test method for
+     * {@link ubic.basecode.gui.table.TableSorter#TableSorter(javax.swing.table.TableModel, ubic.basecode.gui.JMatrixDisplay)}.
      */
     public void testTableSorterTableModelJMatrixDisplay() {
-        fail( "Not yet implemented" );
+        // fail( "Not yet implemented" );
     }
 
 }
