a79a78835a0d846778c09283423b53fd652558a4
ppavlidis <paul@chibi.ubc.ca>
2011-02-15 02:21:07 +0000

    change name of test.

--- test/ubic/basecode/math/KruskalWallisTest.java
+++ test/ubic/basecode/math/KruskalWallisTest.java
@@ -24,7 +24,7 @@ import junit.framework.TestCase;
  */
 public class KruskalWallisTest extends TestCase {
 
-    public void test() {
+    public void testKW() {
 
         double[] s = new double[] { 2.9, 3.0, 2.5, 2.6, 3.2, 3.8, 2.7, 4.0, 2.4, 2.8, 3.4, 3.7, 2.2, 2.0 };
 
