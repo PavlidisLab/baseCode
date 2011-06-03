e7a2c487e2b0ea89cc06fba2f171a3a2fc8d230c
ppavlidis <paul@chibi.ubc.ca>
2011-06-03 22:55:26 +0000

    Human phenotype ontology (2100)

--- src/ubic/basecode/ontology/providers/NIFSTDOntologyService.java
+++ src/ubic/basecode/ontology/providers/NIFSTDOntologyService.java
@@ -1,11 +1,29 @@
+/*
+ * The baseCode project
+ * 
+ * Copyright (c) 2011 University of British Columbia
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 package ubic.basecode.ontology.providers;
 
 import ubic.basecode.ontology.Configuration;
 
+/**
+ * @author paul
+ * @version $Id$
+ */
 public class NIFSTDOntologyService extends AbstractOntologyMemoryBackedService {
 
     private static final String NIFSTD_ONTOLOGY_URL = "url.nifstdOntology";
-    
+
     @Override
     protected String getOntologyName() {
         return "nifstdOntology";
@@ -13,7 +31,7 @@ public class NIFSTDOntologyService extends AbstractOntologyMemoryBackedService {
 
     @Override
     protected String getOntologyUrl() {
-        return Configuration.getString(NIFSTD_ONTOLOGY_URL);
+        return Configuration.getString( NIFSTD_ONTOLOGY_URL );
     }
 
 }
