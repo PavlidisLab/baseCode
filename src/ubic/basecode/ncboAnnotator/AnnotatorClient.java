d4b01ffb0a6ad5d51265a92621051f4b2474b724
nicolas <>
2013-07-22 21:12:23 +0000

    Moving AnnotatorClient and AnnotatorResponse to baseCode (still many points to fix but its a start)

---
+++ src/ubic/basecode/ncboAnnotator/AnnotatorClient.java
@@ -0,0 +1,186 @@
+/*
+ * The Gemma project
+ * 
+ * Copyright (c) 2013 University of British Columbia
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package ubic.basecode.ncboAnnotator;
+
+import java.io.StringReader;
+import java.util.Collection;
+import java.util.TreeSet;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.apache.http.client.fluent.Form;
+import org.apache.http.client.fluent.Request;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+
+import ubic.basecode.ontology.Configuration;
+
+/**
+ * This should behave the same as the web service from this link : http://bioportal.bioontology.org/annotator
+ **/
+public class AnnotatorClient {
+
+    private static String ANNOTATOR_URL = "http://rest.bioontology.org/obs/annotator";
+
+    // this API_KEY needs to be added to basecode.properties
+    // The value I used in my configuration file is : ncbo.api.key=68835db8-b142-4c7d-9509-3c843849ad67
+    private static String API_KEY = Configuration.getString( "ncbo.api.key" );
+
+    private static String ONTOLOGY_USED = "";
+
+    /**
+     * Create the Annotator Client
+     * 
+     * @param ontologiesToUse a list of id representing what Ontology to use
+     */
+    // must let it know what Ontology to search
+    public AnnotatorClient( Collection<Long> ontologiesToUse ) {
+        // 1009 is disease ontology
+        // 1125 is hp ontology
+        // TODO unsure how to use string (ontology name) rather that number
+
+        String ontologiesId = "";
+
+        for ( Long id : ontologiesToUse ) {
+            ontologiesId = id + ",";
+        }
+
+        if ( ontologiesId.length() > 0 ) {
+            // take out last ,
+            ontologiesId.substring( 0, ontologiesId.length() - 1 );
+        }
+        ONTOLOGY_USED = ontologiesId;
+    }
+
+    /**
+     * for a specific term description return a Collection of ontology terms found
+     * 
+     * @param term the description of the term
+     * @return Collection<AnnotatorResponse> the answers found by the nbco annotator
+     */
+    public Collection<AnnotatorResponse> findTerm( String term ) {
+
+        String searchTerm = removeSpecificCharacters( term );
+
+        // TreeSet used: term are ordered with the compareTo method of AnnotatorResponse
+        // 1- exact match from DOID
+        // 2- synonym from DOID
+        // 3- exact match from other resources
+        // 4- synonym from other resources
+        // 5 - other results
+        // to change this rewrite own compareTo method to define required order
+        Collection<AnnotatorResponse> responsesFound = new TreeSet<AnnotatorResponse>();
+
+        try {
+
+            // build the request with desire parameters
+            Request request = Request.Post( ANNOTATOR_URL ).bodyForm(
+                    Form.form().add( "textToAnnotate", searchTerm ).add( "ontologiesToKeepInResult", ONTOLOGY_USED )
+                            .add( "withDefaultStopWords", "true" ).add( "levelMax", "0" ).add( "semanticTypes", "" )
+                            .add( "mappingTypes", "" ).add( "wholeWordOnly", "true" )
+                            .add( "isVirtualOntologyId", "true" ).add( "longestOnly", "false" )
+                            .add( "filterNumber", "true" ).add( "isTopWordsCaseSensitive", "false" )
+                            .add( "mintermSize", "3" ).add( "scored", "true" ).add( "withSynonyms", "true" )
+                            .add( "ontologiesToExpand", "" ).add( "apikey", API_KEY ).build() );
+
+            // Execute the POST method
+            String contents = request.execute().returnContent().asString();
+
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            InputSource is = new InputSource( new StringReader( contents ) );
+            Document document = builder.parse( is );
+
+            NodeList nodes = document.getElementsByTagName( "annotationBean" );
+
+            // for each response receive, populate the return objects
+            for ( int temp = 0; temp < nodes.getLength(); temp++ ) {
+
+                Node nNode = nodes.item( temp );
+
+                Element eElement = ( Element ) nNode;
+
+                // this id represents the ontology used
+                String localOntologyId = eElement.getElementsByTagName( "localOntologyId" ).item( 0 ).getTextContent();
+                String ontologyUsed = "";
+
+                // a number identify what ontology the results is from
+                // TODO might need to add some or find better way to do this
+                if ( localOntologyId.equalsIgnoreCase( "50173" ) ) {
+                    ontologyUsed = "HP";
+                } else if ( localOntologyId.equalsIgnoreCase( "50310" ) ) {
+                    ontologyUsed = "DOID";
+                }
+
+                // score given
+                Integer score = Integer.valueOf( eElement.getElementsByTagName( "score" ).item( 0 ).getTextContent() );
+                // value of phenotype
+                String preferredName = eElement.getElementsByTagName( "preferredName" ).item( 0 ).getTextContent();
+                // valueUri of phenotype
+                String fullId = eElement.getElementsByTagName( "fullId" ).item( 0 ).getTextContent();
+                // create the return representing a results found
+                AnnotatorResponse annotatorResponse = new AnnotatorResponse( score, preferredName, fullId, searchTerm,
+                        ontologyUsed );
+
+                // populates all synonym given for that specific term
+                for ( int i = 0; i < eElement.getElementsByTagName( "synonyms" ).getLength(); i++ ) {
+                    annotatorResponse.addSynonyms( java.net.URLDecoder.decode(
+                            eElement.getElementsByTagName( "synonyms" ).item( i ).getTextContent(), "UTF-8" )
+                            .toLowerCase() );
+                }
+
+                // if the search term used is a synonym found
+                if ( annotatorResponse.getSynonyms().contains( searchTerm.toLowerCase() ) ) {
+                    annotatorResponse.setSynonym( true );
+                }
+
+                // add the reponse found
+                responsesFound.add( annotatorResponse );
+            }
+
+        } catch ( Exception e ) {
+            e.printStackTrace();
+        }
+        return responsesFound;
+    }
+
+    private String removeSpecificCharacters( String txt ) {
+
+        String newTxt = txt.replaceAll( "\\{", "" );
+        newTxt = newTxt.replaceAll( "\\}", "" );
+        newTxt = newTxt.replaceAll( "\\[", "" );
+        newTxt = newTxt.replaceAll( "\\]", "" );
+        newTxt = newTxt.replaceAll( "\\?", "" );
+
+        return newTxt;
+    }
+
+    // These are the setting PP used before, DO only
+    // method.addParameter("withDefaultStopWords","true"); // default is false
+    // method.addParameter("ontologiesToExpand", "1009");
+    // method.addParameter("ontologiesToKeepInResult", "1009");
+    // method.addParameter("isVirtualOntologyId", "true"); // default is false, true is recommended
+    // method.addParameter("levelMax", "10"); // expand to root, 10 is enough.
+    // method.addParameter("mappingTypes", "null"); //null, Automatic, Manual
+    // method.addParameter("textToAnnotate", text);
+    // method.addParameter("format", "tabDelimited"); //Options are 'text', 'xml', 'tabDelimited'
+    // method.addParameter("apikey", "7b14e900-1ba2-4e58-ac21-c5c4c74e7ece") // Paul's
+    // PostMethod method = new PostMethod( annotatorUrl );
+
+}
---
+++ src/ubic/basecode/ncboAnnotator/AnnotatorResponse.java
@@ -0,0 +1,219 @@
+/*
+ * The Gemma project
+ * 
+ * Copyright (c) 2013 University of British Columbia
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package ubic.basecode.ncboAnnotator;
+
+import java.util.HashSet;
+
+/**
+ * This represents an Ontology terms returned by the AnnotatorClient
+ **/
+public class AnnotatorResponse implements Comparable<AnnotatorResponse> {
+
+    private Integer score = 0;
+    private String value = "";
+    private String valueUri = "";
+    private boolean synonym = false;
+
+    // search query used to find this results
+    private String searchQuery = "";
+    // ontology where the result was found
+    private String ontologyUsed = "";
+
+    // list of synonyms associated with this valueUri
+    private HashSet<String> synonyms = new HashSet<String>();
+
+    public AnnotatorResponse( Integer score, String value, String valueUri, String searchQuery, String ontologyUsed ) {
+        super();
+        this.score = score;
+        this.value = value;
+        this.valueUri = valueUri;
+        this.searchQuery = searchQuery;
+        this.ontologyUsed = ontologyUsed;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore( Integer score ) {
+        this.score = score;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue( String value ) {
+        this.value = value;
+    }
+
+    public String getValueUri() {
+        return valueUri;
+    }
+
+    public void setValueUri( String valueUri ) {
+        this.valueUri = valueUri;
+    }
+
+    public String getSearchQuery() {
+        return searchQuery;
+    }
+
+    public void setSearchQuery( String searchQuery ) {
+        this.searchQuery = searchQuery;
+    }
+    
+    // term are ordered given priority to DOID terms
+    // 1- exact match from DOID
+    // 2- synonym from DOID
+    // 3- exact match from other resources
+    // 4- synonym from other resources
+    // 5 - other results
+    // to change this rewrite own compareTo method to define required order
+    @Override
+    public int compareTo( AnnotatorResponse annotatorResponse ) {
+
+        boolean exactMatch = isExactMatch();
+        boolean exactMatchCompare = annotatorResponse.isExactMatch();
+        boolean isSynonyme = isSynonym();
+        boolean isSynonymeCompare = annotatorResponse.isSynonym();
+        boolean diseaseOntology = isDiseaseUsed();
+        boolean diseaseCompare = annotatorResponse.isDiseaseUsed();
+
+        if ( diseaseOntology ) {
+
+            if ( exactMatch ) {
+                return -1;
+            } else if ( isSynonyme ) {
+                if ( !diseaseCompare ) {
+                    return -1;
+                } else if ( !exactMatchCompare ) {
+                    return -1;
+                }
+            }
+        }
+        // hp
+        else if ( exactMatch || isSynonyme ) {
+            if ( diseaseCompare ) {
+                if ( exactMatchCompare || isSynonymeCompare ) {
+                    return 1;
+                }
+            }
+
+            // hp vs hp
+            if ( exactMatch ) {
+                return -1;
+            } else if ( isSynonyme && !exactMatchCompare ) {
+                return -1;
+            }
+        }
+        return 1;
+    }
+
+    @Override
+    public String toString() {
+        return "AnnotatorResponse [score=" + score + ", value=" + value + ", valueUri=" + valueUri + ", searchQuery="
+                + searchQuery + "]";
+    }
+
+    public HashSet<String> getSynonyms() {
+        return synonyms;
+    }
+
+    public void setSynonyms( HashSet<String> synonyms ) {
+        this.synonyms = synonyms;
+    }
+
+    
+    // this information is given as a long string with space and new lines, parse and add all synonyms
+    public void addSynonyms( String line ) {
+
+        String[] tokens = line.split( "\n" );
+
+        for ( String token : tokens ) {
+
+            token = token.trim();
+
+            if ( !token.isEmpty() ) {
+                this.synonyms.add( token );
+            }
+        }
+    }
+
+    public boolean isSynonym() {
+        return synonym;
+    }
+
+    public boolean isDiseaseUsed() {
+        if ( ontologyUsed.equalsIgnoreCase( "DOID" ) ) {
+            return true;
+        }
+        return false;
+    }
+
+    public void setSynonym( boolean synonym ) {
+        this.synonym = synonym;
+    }
+
+    public boolean isExactMatch() {
+
+        if ( this.value.equalsIgnoreCase( this.searchQuery ) ) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public String getOntologyUsed() {
+        return ontologyUsed;
+    }
+
+    public void setOntologyUsed( String ontologyUsed ) {
+        this.ontologyUsed = ontologyUsed;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ( ( ontologyUsed == null ) ? 0 : ontologyUsed.hashCode() );
+        result = prime * result + ( ( score == null ) ? 0 : score.hashCode() );
+        result = prime * result + ( ( searchQuery == null ) ? 0 : searchQuery.hashCode() );
+        result = prime * result + ( ( valueUri == null ) ? 0 : valueUri.hashCode() );
+        return result;
+    }
+
+    @Override
+    public boolean equals( Object obj ) {
+        if ( this == obj ) return true;
+        if ( obj == null ) return false;
+        if ( getClass() != obj.getClass() ) return false;
+        AnnotatorResponse other = ( AnnotatorResponse ) obj;
+        if ( ontologyUsed == null ) {
+            if ( other.ontologyUsed != null ) return false;
+        } else if ( !ontologyUsed.equals( other.ontologyUsed ) ) return false;
+        if ( score == null ) {
+            if ( other.score != null ) return false;
+        } else if ( !score.equals( other.score ) ) return false;
+        if ( searchQuery == null ) {
+            if ( other.searchQuery != null ) return false;
+        } else if ( !searchQuery.equals( other.searchQuery ) ) return false;
+        if ( valueUri == null ) {
+            if ( other.valueUri != null ) return false;
+        } else if ( !valueUri.equals( other.valueUri ) ) return false;
+        return true;
+    }
+
+}
