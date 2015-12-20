e389946d5b0e12285b84784421e418dfc6ffcd55
ppavlidis <paul@chibi.ubc.ca>
2015-12-20 19:36:11 +0000

    uberon (bug 4278)

---
+++ src/ubic/basecode/ontology/providers/UberonOntologyService.java
@@ -0,0 +1,54 @@
+/*
+ * The baseCode project
+ * 
+ * Copyright (c) 2015 University of British Columbia
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package ubic.basecode.ontology.providers;
+
+import ubic.basecode.util.Configuration;
+
+/**
+ * Uberon
+ * 
+ * @author paul
+ * @version $Id$
+ */
+public class UberonOntologyService extends AbstractOntologyMemoryBackedService {
+
+    private static final String ONTOLOGY_URL = "url.uberonOntology";
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see ubic.basecode.ontology.providers.AbstractOntologyService#getOntologyName()
+     */
+    @Override
+    protected String getOntologyName() {
+        return "uberon";
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see ubic.basecode.ontology.providers.AbstractOntologyService#getOntologyUrl()
+     */
+    @Override
+    protected String getOntologyUrl() {
+        return Configuration.getString( ONTOLOGY_URL );
+    }
+
+}
