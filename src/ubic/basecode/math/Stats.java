3edeef22624b2df1ddaa714c06d216709f140533
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2025-03-02 10:19:11 -0800

    Optimize DescriptiveWithMissing routines
    Avoid copies unless absolutely necessary.
    
    Take advantage of sorting behavior to remove missing values in-place.
    
    Remove unsupported operations and do not inherit from Descriptive. This
    way, unsupported methods will simply not exist.

--- src/ubic/basecode/math/Stats.java
+++ src/ubic/basecode/math/Stats.java
@@ -23,7 +23,6 @@ import java.util.Set;
 
 import cern.colt.list.DoubleArrayList;
 import cern.jet.stat.Descriptive;
-import org.apache.commons.math3.util.DoubleArray;
 
 /**
  * Miscellaneous functions used for statistical analysis. Some are optimized or specialized versions of methods that can
